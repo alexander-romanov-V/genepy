c4218dc6ceeb2b7e0476eb11d91797a354c56dea
alexander-romanov-V <romanovav@ya.ru>
2025-05-06 01:00:36 +0300

    6

---
+++ b11_counting_words.py
@@ -0,0 +1,9 @@
+"""BASIC - Counting Words"""
+
+whetting_your_appetite = "Python is an easy to learn, powerful programming language. It has efficient high level data structures and a simple but effective approach to object oriented programming. This tutorial introduces the reader informally to the basic concepts and features of the Python language and system. For a description of standard objects and modules..."
+
+# Solution 1
+print(len(whetting_your_appetite.split()))
+
+# Solution 2 - use RegEx
+print(len(__import__("re").findall(r"[a-zA-Z]+", whetting_your_appetite)))
