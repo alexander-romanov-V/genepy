68eebfbb149d4f606fff991438ea1dc1f64b1bde
alexander-romanov-V <romanovav@ya.ru>
2025-05-08 00:02:36 +0300

    15

---
+++ b21_print_sorbet_flavors.py
@@ -0,0 +1,18 @@
+"""BASIC - Print sorbet flavors"""
+
+FLAVORS = [
+    "Banana",
+    "Chocolate",
+    "Lemon",
+    "Pistachio",
+    "Raspberry",
+    "Strawberry",
+    "Vanilla",
+]
+
+# Solution 1
+from itertools import starmap, combinations
+print(*starmap(lambda a, b: a + ", " + b, combinations(FLAVORS, 2)), sep="\n")
+
+# Solution 2
+print(*(", ".join(c) for c in __import__("itertools").combinations(FLAVORS, 2)), sep="\n")
