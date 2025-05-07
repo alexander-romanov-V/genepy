cf09fb86563718dac2cd6738e44db2d895f98b4b
alexander-romanov-V <romanovav@ya.ru>
2025-05-07 12:11:20 +0300

    9

---
+++ b22_distance.py
@@ -0,0 +1,38 @@
+"""BASIC - Distance"""
+
+
+# Solution 1
+def dist(points):
+    """Returns max distance"""
+    return max(points) - min(points)
+
+
+# Solution 2
+def dist2(points):
+    """Returns max distance"""
+    mini, *_, maxi = sorted(points)
+    return maxi - mini
+
+
+# Solution 3 (just for fun)
+dist3 = lambda points: max(points) - min(points)
+
+
+if __name__ == "__main__":
+    print(dist([1, 2, 3]))
+    print(dist([1, 2, 3, 2.5]))
+    print(dist([1, 2, 3, 2.5, 3.5]))
+    print(dist([1, 2, 3, 2.5, 3.5, 120]))
+    print(dist([1, 2, 3, 2.5, 3.5, 120, -1000]))
+
+    print(dist2([1, 2, 3]))
+    print(dist2([1, 2, 3, 2.5]))
+    print(dist2([1, 2, 3, 2.5, 3.5]))
+    print(dist2([1, 2, 3, 2.5, 3.5, 120]))
+    print(dist2([1, 2, 3, 2.5, 3.5, 120, -1000]))
+
+    print(dist3([1, 2, 3]))
+    print(dist3([1, 2, 3, 2.5]))
+    print(dist3([1, 2, 3, 2.5, 3.5]))
+    print(dist3([1, 2, 3, 2.5, 3.5, 120]))
+    print(dist3([1, 2, 3, 2.5, 3.5, 120, -1000]))
