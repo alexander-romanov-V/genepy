0fd67f33b70b1ec0eb68e97be50e01fcbfda5690
alexander-romanov-V <romanovav@ya.ru>
2025-05-14 10:53:10 +0300

    40

--- m16_perfect_deck_shuffle.py
+++ m16_perfect_deck_shuffle.py
@@ -22,13 +22,14 @@ def perfect_shuffle2(deck: list):
 def perfect_shuffle3(deck: list):
     """Splitting a deck of cards into equal halves, and perfectly interleaving them."""
     l = len(deck) // 2
-    return [c for x in zip(deck[:l], deck[l:]) for c in x]
+    return [c for tup in zip(deck[:l], deck[l:]) for c in tup]
 
 
 # Solution 4
 def perfect_shuffle4(deck: list):
     """Splitting a deck of cards into equal halves, and perfectly interleaving them."""
-    shuffled, l = deck[:], len(deck) // 2
+    l = len(deck) // 2
+    shuffled = deck
     shuffled[::2], shuffled[1::2] = deck[:l], deck[l:]
     return shuffled
 
@@ -40,9 +41,31 @@ def perfect_shuffle5(deck: list):
     return [deck[i] for r in range(l) for i in (r, r + l)]
 
 
+# Solution 6
+def perfect_shuffle6(deck: list):
+    """Splitting a deck of cards into equal halves, and perfectly interleaving them."""
+    l = len(deck) // 2
+    return list(sum(zip(deck[:l], deck[l:]), ()))
+    # 1. zip the lists together. (using zip)
+    # 2. flatten to a tuple (using sum(..., ())
+    # 3. convert to a list
+
+
 if __name__ == "__main__":
-    assert perfect_shuffle([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
-    assert perfect_shuffle2([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
-    assert perfect_shuffle3([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
-    assert perfect_shuffle4([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
-    assert perfect_shuffle5([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle2([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle3([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle4([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle5([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+    # assert perfect_shuffle6([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+
+    for p in [
+        perfect_shuffle,
+        perfect_shuffle2,
+        perfect_shuffle3,
+        perfect_shuffle4,
+        perfect_shuffle5,
+        perfect_shuffle6,
+    ]:
+        assert p([1, 2, 3, 4, 5, 6]) == [1, 4, 2, 5, 3, 6]
+        print(f"{p.__name__:20} \033[92m[ PASS ]\033[0m")
