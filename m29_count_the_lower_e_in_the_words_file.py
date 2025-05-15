4b429e92ea8e447b205dd59f9285a5beeedc505c
alexander-romanov-V <romanovav@ya.ru>
2025-05-16 00:01:44 +0300

    53

---
+++ m29_count_the_lower_e_in_the_words_file.py
@@ -0,0 +1,13 @@
+"""MEDIUM - Count the lower 'e' in the 'words' file"""
+
+# Solution 1 - my
+with open("words.txt", "r", encoding="UTF-8") as file:
+    print(file.read().count("e"))
+
+
+# Solution 2 - short version
+print(open("words.txt").read().count("e"))
+
+
+# Solution 3
+print(__import__("pathlib").Path("words.txt").read_text().count("e"))
