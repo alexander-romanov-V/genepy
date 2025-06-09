291ab41f969b0448005fbbf7ac60049ce1fc53a4
alexander-romanov-V <romanovav@ya.ru>
2025-06-09 13:14:11 +0300

    86

--- h13_password_generator.py
+++ h13_password_generator.py
@@ -29,8 +29,28 @@ def pwgen(length: int, with_digits=True, with_uppercase=True) -> str:
             return "".join(res)
 
 
+# Solution 2 - my second
+from random import choices, choice, shuffle
+from string import ascii_lowercase, ascii_uppercase, digits
+def pwgen2(length, with_digits=True, with_uppercase=True):
+    """Returns a password with lowercase letters"""
+    res = choices(ascii_lowercase + digits * with_digits + ascii_uppercase * with_uppercase, k=length - with_digits - with_uppercase - 1)
+    res += choice(ascii_lowercase)
+    if with_digits:
+        res += choice(digits)
+    if with_uppercase:
+        res += choice(ascii_uppercase)
+    shuffle(res)
+    return "".join(res)
+
+
 if __name__ == "__main__":
     print(pwgen(6, False, False))
     print(pwgen(6, False, True))
     print(pwgen(6, True, False))
     print(pwgen(6, True, True))
+
+    print(pwgen2(6, False, False))
+    print(pwgen2(6, False, True))
+    print(pwgen2(6, True, False))
+    print(pwgen2(6, True, True))
