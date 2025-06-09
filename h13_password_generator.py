44b067f6408ccd01679090d750139ee7a883be30
alexander-romanov-V <romanovav@ya.ru>
2025-06-09 13:14:50 +0300

    87

--- h13_password_generator.py
+++ h13_password_generator.py
@@ -6,18 +6,6 @@ from string import ascii_lowercase, ascii_uppercase, digits
 def pwgen(length: int, with_digits=True, with_uppercase=True) -> str:
     """
     Returns a password with lowercase letters
-    AND digits if with_digits == True
-    AND uppercase letters if with_uppercase == True
-
-    :param length: the length of the generated password
-    :type length: int
-    :param with_digits: Defaulting to True, to allow or disallow digits
-    :type with_digits: bool
-    :param with_uppercase: Defaulting to True, to allow or disallow capital letters
-    :type with_uppercase: bool
-
-    :return: A string with random password
-    :rtype: str
     """
     while True:
         res = choices(ascii_lowercase + digits * with_digits + ascii_uppercase * with_uppercase, k=length)
