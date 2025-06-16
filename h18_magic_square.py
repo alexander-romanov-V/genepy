397ae1911df22f4e226ce157221960def3cd595f
alexander-romanov-V <romanovav@ya.ru>
2025-06-16 05:49:54 +0300

    116

--- h18_magic_square.py
+++ h18_magic_square.py
@@ -4,33 +4,40 @@ import numpy as np
 
 
 # Solution 1 - my first
-def fill_magic_square(square: np.array) -> None:
-    ...
+def fill_magic_square(square: np.array) -> None:  # type: ignore
+    """Fill 0's in magic square"""
+    n = square.shape[0]
+    # m = n * (n**2 + 1) // 2 # Only for normal magic square
+    m = 0
+    for j in range(n):
+        if np.count_nonzero(square[j, :]) == n:
+            m = sum(square[j, :])
+            break
+        elif np.count_nonzero(square[:, j]) == n:
+            m = sum(square[:, j])
+            break
+    if m == 0:
+        if np.count_nonzero(square.diagonal()) == n:
+            m = sum(square.diagonal())
+        elif np.count_nonzero(np.flipud(square).diagonal()) == n:
+            m = sum(np.flipud(square).diagonal())
+        else:
+            raise ValueError("Can't find magic square number - too many blanks")
+
+    while np.count_nonzero(square) != n**2:
+        for j in range(n):
+            for i in range(n):
+                if square[j, i] == 0:
+                    if np.count_nonzero(square[j, :]) == n - 1:
+                        square[j, i] = m - sum(square[j, :])
+                    elif np.count_nonzero(square[:, i]) == n - 1:
+                        square[j, i] = m - sum(square[:, i])
+                    elif i == j and np.count_nonzero(square.diagonal()) == n - 1:
+                        square[j, i] = m - sum(square.diagonal())
+                    elif (
+                        i == n - 1 - j
+                        and np.count_nonzero(np.flipud(square).diagonal()) == n - 1
+                    ):
+                        square[j, i] = m - sum(np.flipud(square).diagonal())
 
 
-if __name__ == "__main_":
-    easy_square = np.array([
-        [2, 7, 6],
-        [9, 0, 1],
-        [4, 3, 8],
-    ])
-    fill_magic_square(easy_square)
-    print(easy_square)
-    # Output:
-    # array([[ 2, 7, 6 ],
-    #    [ 9, 5, 1 ],
-    #    [ 4, 3, 8 ]]])
-
-    harder_square = np.array([
-        [ 4,  0, 15,  0],
-        [ 9,  0,  6, 12],
-        [ 5, 11, 10,  0],
-        [16,  0,  0, 13],
-    ])
-    fill_magic_square(harder_square)
-    print(harder_square)
-    # Output:
-    # array([[4, 14, 15,  1],
-    #    [9,  7,  6, 12],
-    #    [5, 11, 10,  8],
-    #    [16, 2,  3, 13]])
