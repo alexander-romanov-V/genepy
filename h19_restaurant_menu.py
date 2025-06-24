1aa26b2ec9f03448c2a273b6d4fd01de8f7139ba
alexander-romanov-V <romanovav@ya.ru>
2025-06-24 11:38:10 +0300

    140

--- h19_restaurant_menu.py
+++ h19_restaurant_menu.py
@@ -1,19 +1,15 @@
 """HARD - Restaurant menu"""
 
 # Solution 1 - my first
-from enum import Enum, unique
 
-
-@unique
-class DishType(Enum):
-    starter = 1
-    dish = 2
-    dessert = 3
+DishType = ["starter", "dish", "dessert"]
 
 
 class Dish:
-    def __init__(self, name: str, preparation_time: int, dish_type: DishType) -> None:
+    def __init__(self, name: str, preparation_time: int, dish_type: str) -> None:
         self.name = name
+        if dish_type not in DishType:
+            raise ValueError("dish_type can be only: " + ", ".join(DishType))
         self.preparation_time = preparation_time
         self.dish_type = dish_type
 
@@ -45,34 +41,53 @@ class Menu:
         self.dishes.append(dish)
 
     def get_starters(self) -> list[Dish]:
-        return [d for d in self.dishes if d.dish_type == DishType.starter]
+        return [d for d in self.dishes if d.dish_type == "starter"]
 
     def get_dishes(self) -> list[Dish]:
-        return [d for d in self.dishes if d.dish_type == DishType.dish]
+        return [d for d in self.dishes if d.dish_type == "dish"]
 
     def get_desserts(self) -> list[Dish]:
-        return [d for d in self.dishes if d.dish_type == DishType.dessert]
+        return [d for d in self.dishes if d.dish_type == "dessert"]
 
-    def get_minimum_preparation_time(self):
-        starters_time = min(self.get_starters())
-        dishes_time = min(self.get_dishes())
-        desserts_time = min(self.get_desserts())
-        return starters_time.preparation_time + dishes_time.preparation_time + desserts_time.preparation_time
+    def get_minimum_preparation_time(self) -> int:
+        return (
+            min([d.preparation_time for d in self.get_starters()], default=0)
+            + min([d.preparation_time for d in self.get_dishes()], default=0)
+            + min([d.preparation_time for d in self.get_desserts()], default=0)
+        )
 
-    def get_maximum_preparation_time(self):
-        starters_time = max(self.get_starters())
-        dishes_time = max(self.get_dishes())
-        desserts_time = max(self.get_desserts())
-        return starters_time.preparation_time + dishes_time.preparation_time + desserts_time.preparation_time
+    def get_maximum_preparation_time(self) -> int:
+        return (
+            max([d.preparation_time for d in self.get_starters()], default=0)
+            + max([d.preparation_time for d in self.get_dishes()], default=0)
+            + max([d.preparation_time for d in self.get_desserts()], default=0)
+        )
 
     def __add__(self, other):
         menu = Menu(self.name + " & " + other.name)
         menu.dishes = self.dishes.copy() + other.dishes.copy()
         return menu
-    
+
+    def __str__(self) -> str:
+        return (
+            "STARTER\n"
+            + "\n".join(d.name for d in self.get_starters())
+            + "\n\nDISH\n"
+            + "\n".join(d.name for d in self.get_dishes())
+            + "\n\nDESSERT\n"
+            + "\n".join(d.name for d in self.get_desserts())
+        )
+
 
 if __name__ == "__main__":
     menu_1 = Menu("One")
+    menu_1.add_dish(Dish("eggs & mayonaise", 5, "starter"))
+    menu_1.add_dish(Dish("burger", 15, "dish"))
+    menu_1.add_dish(Dish("waffle", 20, "dessert"))
     menu_2 = Menu("Two")
+    menu_2.add_dish(Dish("salad", 10, "starter"))
+    menu_2.add_dish(Dish("pizza", 20, "dish"))
+    menu_2.add_dish(Dish("chocolate cookie", 30, "dessert"))
     menu_3 = menu_1 + menu_2
     print(menu_3.name)
+    print(menu_3)
