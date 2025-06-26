fa791819173976e49f65c7e8dd906aa751e4b9d8
alexander-romanov-V <romanovav@ya.ru>
2025-06-26 11:50:03 +0300

    148

--- h19_restaurant_menu3.py
+++ h19_restaurant_menu3.py
@@ -2,12 +2,13 @@
 
 # Solution 3
 
+
 class Dish:
     def __init__(self, name, preparation_time, dish_type):
         self.name = name
         self.preparation_time = preparation_time
         self.dish_type = dish_type
-    
+
     def __eq__(self, other):
         return self.preparation_time == other.preparation_time
 
@@ -16,7 +17,7 @@ class Dish:
 
     def __ge__(self, other):
         return self.preparation_time >= other.preparation_time
-    
+
     def __lt__(self, other):
         return not self.__gt__(other)
 
@@ -32,30 +33,38 @@ class Menu:
     def __str__(self):
         menu = [[k.upper()] for k in self.food.keys()]
         for i, dish in enumerate(self.food.values()):
-            menu[i].extend(d.name for d in sorted(dish, key=lambda d: d.preparation_time))
+            menu[i].extend(
+                d.name for d in sorted(dish, key=lambda d: d.preparation_time)
+            )
         return "\n\n".join("\n".join(dish) for dish in menu)
 
     def __add__(self, other):
         menu = {k: self.food[k] + other.food[k] for k in self.food.keys()}
         return Menu(f"{self.name} & {other.name}", menu)
-      
+
     def add_dish(self, dish):
         self.food[dish.dish_type].append(dish)
-    
+
     def get_starters(self):
         return self.food["starter"]
-    
+
     def get_dishes(self):
         return self.food["dish"]
-    
+
     def get_desserts(self):
         return self.food["dessert"]
-    
+
     def get_minimum_preparation_time(self):
-        return sum(min((d.preparation_time for d in dish), default=0) for dish in self.food.values())
+        return sum(
+            min((d.preparation_time for d in dish), default=0)
+            for dish in self.food.values()
+        )
 
     def get_maximum_preparation_time(self):
-        return sum(max((d.preparation_time for d in dish), default=0) for dish in self.food.values())
+        return sum(
+            max((d.preparation_time for d in dish), default=0)
+            for dish in self.food.values()
+        )
 
 
 if __name__ == "__main__":
