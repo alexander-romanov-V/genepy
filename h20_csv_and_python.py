d7a68fbce4c47ae8ce5b2a9bc2d5fade0e207381
alexander-romanov-V <romanovav@ya.ru>
2025-06-29 14:15:22 +0300

    158

--- h20_csv_and_python.py
+++ h20_csv_and_python.py
@@ -42,7 +42,7 @@ def parse_csv():
 
 
 if __name__ == "__main__":
-    import datetime
+    # import datetime
 
     # from <your_solution> import generate_csv
     meteo = [
@@ -59,32 +59,47 @@ if __name__ == "__main__":
             ("weather", "cloudy"),
         ),
     ]
+    
     generate_csv(meteo)
 
     # from <your_solution> import parse_csv
-    print(parse_csv())
-    # [{'Birthdate': datetime.date(1815, 12, 10),
-    # 'Comments': 'The first one',
-    # 'Firstname': 'Ada',
-    # 'Lastname': 'Lovelace',
-    # 'Marks': [4242, 1010]},
-    # {'Birthdate': datetime.date(1969, 12, 28),
-    # 'Comments': 'Have a problem with penguin',
-    # 'Firstname': 'Linus',
-    # 'Lastname': 'Torvald',
-    # 'Marks': [42, 21]},
-    # {'Birthdate': datetime.date(1968, 5, 19),
-    # 'Comments': 'This guy is just crazy',
-    # 'Firstname': 'Theo',
-    # 'Lastname': 'De Raadt',
-    # 'Marks': [18, 19, 20]},
-    # {'Birthdate': datetime.date(1941, 9, 9),
-    # 'Comments': 'Like a boss',
-    # 'Firstname': 'Dennis',
-    # 'Lastname': 'Ritchie',
-    # 'Marks': [20, 20, 20]},
-    # {'Birthdate': datetime.date(1912, 6, 23),
-    # 'Comments': "Shouldn't eat apple",
-    # 'Firstname': 'Alan',
-    # 'Lastname': 'Turing',
-    # 'Marks': [42, 42, 42]}]
+    students = [
+        {
+            "Birthdate": datetime.date(1815, 12, 10),
+            "Comments": "The first one",
+            "Firstname": "Ada",
+            "Lastname": "Lovelace",
+            "Marks": [4242, 1010],
+        },
+        {
+            "Birthdate": datetime.date(1969, 12, 28),
+            "Comments": "Have a problem with penguin",
+            "Firstname": "Linus",
+            "Lastname": "Torvald",
+            "Marks": [42, 21],
+        },
+        {
+            "Birthdate": datetime.date(1968, 5, 19),
+            "Comments": "This guy is just crazy",
+            "Firstname": "Theo",
+            "Lastname": "De Raadt",
+            "Marks": [18, 19, 20],
+        },
+        {
+            "Birthdate": datetime.date(1941, 9, 9),
+            "Comments": "Like a boss",
+            "Firstname": "Dennis",
+            "Lastname": "Ritchie",
+            "Marks": [20, 20, 20],
+        },
+        {
+            "Birthdate": datetime.date(1912, 6, 23),
+            "Comments": "Shouldn't eat apple",
+            "Firstname": "Alan",
+            "Lastname": "Turing",
+            "Marks": [42, 42, 42],
+        },
+    ]
+
+    assert parse_csv() == students
+    
