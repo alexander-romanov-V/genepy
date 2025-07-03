887aa29716ab23532e5556551960687acb65ef27
alexander-romanov-V <romanovav@ya.ru>
2025-07-03 10:28:09 +0300

    173

--- h21_sequence_mining.py
+++ h21_sequence_mining.py
@@ -31,6 +31,26 @@ def seq_mining(data: list[str], min_p: float, max_l: int) -> Counter[str]:
     return +res
 
 
+# Solution 2 - my second
+def seq_mining2(seqs: list[str], min_p: float, max_l: int) -> Counter[str]:
+    """Find number of sequences according passed patterns"""
+    res = sum([Counter({d[i : i + seq_l + 1] for seq_l in range(min(max_l, len(d))) for i in range(len(d) - seq_l)}) for d in seqs], Counter())
+    return Counter(c for c in res.elements() if res[c] >= len(seqs) * min_p)
+
+
+# Solution 3
+def seq_mining3(seqs: list, ratio: float, max_len: int):
+    """Find number of sequences according passed patterns"""
+    mine = []
+    size = min(max(len(s) for s in seqs), max_len)
+
+    for seq in seqs:
+        entry = {seq[i : i + j] for j in range(1, size + 1) for i, _ in enumerate(seq)}
+        mine.append(list(entry))
+    count = Counter(s for seq in mine for s in seq)
+    return Counter(c for c in count.elements() if count[c] >= ratio * len(seqs))
+
+
 if __name__ == "__main__":
     data = ["ABCD", "ABABC", "BCAABCD"]
 
@@ -38,6 +58,15 @@ if __name__ == "__main__":
     result2 = {"A": 3, "AB": 3, "ABC": 3, "ABCD": 2, "B": 3, "BC": 3, "BCD": 2, "C": 3, "CD": 2, "D": 2}
     result3 = {"A": 3, "AB": 3, "B": 3, "BC": 3, "C": 3, "CD": 2, "D": 2}
 
-    assert seq_mining(data, 0.34, 3) == result1
-    assert seq_mining(data, 0.34, 4) == result2
-    assert seq_mining(data, 0.50, 2) == result3
+    for p in [
+        seq_mining,
+        seq_mining2,
+        seq_mining3,
+    ]:
+        assert p(data, 0.34, 3) == result1
+        print(f"{p.__name__:20} \033[92m[ PASS 1 ]\033[0m")
+        assert p(data, 0.34, 4) == result2
+        print(f"{p.__name__:20} \033[92m[ PASS 2 ]\033[0m")
+        assert p(data, 0.50, 2) == result3
+        print(f"{p.__name__:20} \033[92m[ PASS 3 ]\033[0m")
+        print()
