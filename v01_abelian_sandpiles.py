2992a0ebf739052cc526233ab3cec17f61803765
alexander-romanov-V <romanovav@ya.ru>
2025-07-08 11:20:18 +0300

    194

--- v01_abelian_sandpiles.py
+++ v01_abelian_sandpiles.py
@@ -1,4 +1,4 @@
-"""HARD - Abelian sandpiles"""
+"""VERY HARD - Abelian sandpiles"""
 
 import numpy as np
 
@@ -10,8 +10,8 @@ def apply_gravity(sandpile):
     * Any "pile" that has 4 or more sand particles on it collapses, resulting in four
       particles being subtracted from the pile and distributed among its neighbors.
     """
-    while np.any(sandpile >= 4):
-        for x, y in list(zip(*np.where(sandpile >= 4))):
+    while idxs := list(zip(*np.where(sandpile >= 4))):
+        for x, y in idxs:
             sandpile[x, y] -= 4
             for xx, yy in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
                 sandpile[xx, yy] += 1
