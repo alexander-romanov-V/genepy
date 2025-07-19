f4d5dba6ae442070277661c423ea296f3c4b2ee6
alexander-romanov-V <romanovav@ya.ru>
2025-07-19 20:25:51 +0300

    234

--- v04_fir_tree.py
+++ v04_fir_tree.py
@@ -1,5 +1,19 @@
 """VERY HARD - Fir tree
 
+You'll write a Python 3 program drawing a fir tree.
+
+The size of the drawed fir tree will depend on the single parameter given to your program. As always, print a human error message if no parameter are given to your program.
+
+Given 0, your program should display nothing.
+Advice
+
+You can test is for several values of the parameter here:
+
+https://sapin.hackinscience.org/?n=10
+
+https://sapin.hackinscience.org/?n=5
+
+etc...
 
 """
 
