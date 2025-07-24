2e23b76a9e0962ef89391da915c988be1763541a
alexander-romanov-V <romanovav@ya.ru>
2025-07-24 16:57:06 +0300

    251

--- v04_fir_tree.py
+++ v04_fir_tree.py
@@ -16,31 +16,42 @@ etc...
 
 
 N                           1   2   3   4   5   6   7   8   9   10
-Blocks                      1   2   3   4   5   6   7   8   9   10  = N
-Lines in Nth block          4   5   6   7   8   9   10  11  12  13  = N+3
-Wfirst_lines in 1th block  [1]  5   11  17  25  33  43  53  65  77  = x = y[-1] - N//2*2  (x = 1 for N==1, y[-1] - y of previous block)
-Wlast_lines in Nth block    7   13  21  29  39  49  61  73  87  101 = y = x+2*(N+2)
-Wbar                        1   3   3   5   5   7   7   9   9   11  = N//2*2+1
-Hbar                        1   2   3   4   5   6   7   8   9   10  = N
+Blocks                      1   2   3   4   5   6   7   8   9   10      = N
+Lines in Nth block          4   5   6   7   8   9   10  11  12  13      = N+3
+Wfirst_lines in 1th block  [1]  5   11  17  25  33  43  53  65  77  = x = y[-1] - N//2*2  (x = 1 for N==1, y[-1] -> y of previous block)
+Wlast_lines in Nth block    7   13  21  29  39  49  61  73  87  101 = y = x + 2*(N+2)
+Wbar                        1   3   3   5   5   7   7   9   9   11      = 1 + N//2*2
+Hbar                        1   2   3   4   5   6   7   8   9   10      = N
 
 """
-
-
-
 
 # Solution 1 - my first
 
 from sys import argv
 
 
-def fir_tree(n: int): ...
+def fir_tree(n: int):
+    w1 = []
+    y = 1
+    for i in range(n):
+        x = y - (i + 1) // 2 * 2
+        y = x + 2 * (i + 3)
+        w1.append(x)
+    wm = w1[-1] + 2 * (n + 2)
+    wb = 1 + n // 2 * 2
+    for b in range(n):
+        for bl in range(w1[b], w1[b] + 2 * (b + 4), 2):
+            print(f"{'*'*bl:^{wm}}")
+    for b in range(n):
+        print(f"{'|'*wb:^{wm}}")
 
 
 if __name__ == "__main__":
-    try:
-        n = int(argv[1])
-    except (IndexError, ValueError):
-        print(f"Usage: {argv[0]} N\n    N - size of fir tree\n")
-        exit()
-    if n > 0:
-        fir_tree(n)
+    fir_tree(4)
+    # try:
+    #     n = int(argv[1])
+    # except (IndexError, ValueError):
+    #     print(f"Usage: {argv[0]} N\n    N - size of fir tree\n")
+    #     exit()
+    # if n > 0:
+    #     fir_tree(n)
