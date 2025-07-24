d14e2f51b3ceb275ffddcbd612492d07bc5a8a06
alexander-romanov-V <romanovav@ya.ru>
2025-07-25 00:15:21 +0300

    254

---
+++ v04_fir_tree2.py
@@ -0,0 +1,33 @@
+"""VERY HARD - Fir tree"""
+
+# Solution 2
+
+import sys
+
+try:
+    n = int(sys.argv[1])
+except (IndexError, ValueError):
+    print(f"Wrong or missing parameter, usage: {sys.argv[0]} INTEGER")
+    sys.exit()
+
+if n == 0:
+    sys.exit()
+
+r = []
+l = 0
+d = 0
+
+for i in range(n):
+    for j in range(4 + i):
+        r += ["*" * (l - l % 2 + 1 + j * 2)]
+    l = len(r[-1]) - int(d * 2 + 2)
+    d += 0.5
+
+
+for i in range(n):
+    r += ["|" * (n + 1 - n % 2)]
+
+lmax = max(map(len, r))
+
+for l in r:
+    print(l.center(lmax).rstrip())
