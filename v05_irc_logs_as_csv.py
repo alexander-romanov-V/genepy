709c02e21d9a951e0bb08f2c611e9120d2b5361a
alexander-romanov-V <romanovav@ya.ru>
2025-07-30 11:02:06 +0300

    266

--- v05_irc_logs_as_csv.py
+++ v05_irc_logs_as_csv.py
@@ -5,7 +5,7 @@ https://github.com/francejs/irc-bot
 Then, concatenate all logs files in the logs folder into a single francejs.csv
 file. Just for your information, this file should contain more than 36,000 lines.
 
-You'll write a program parsing this file (using the csv Python module), to find 
+You'll write a program parsing this file (using the csv Python module), to find
 out and print which pair of nicknames mention each other the most, like:
 
 $ python3 solution.py
@@ -14,10 +14,10 @@ mdk, sizeof
 Beware, I may test with another dataset (of the exact same format).
 Data format of this csv file
 
-There's 4 columns: 
-- Message type (1 means it's a message, other values means it's a system info, we ignore them) 
-- Timestamp of the message (number of seconds since 1st jan 1970 UTC) 
-- Username involved 
+There's 4 columns:
+- Message type (1 means it's a message, other values means it's a system info, we ignore them)
+- Timestamp of the message (number of seconds since 1st jan 1970 UTC)
+- Username involved
 - Actual message
 
 For example, this line has no interest, it simply logs the arrival of a user,
@@ -36,12 +36,11 @@ Here, user _kud mentions user nfroidure on the third line, that's what we're sea
 # Solution 1 - my first
 
 if __name__ == "__main__":
-    
+
     """
     load francejs.csv
-    filter only messagetype == 1 from column #2
+    filter only messagetype == 1 from column #1
     create set of usernames from column #3
     find pairs user - another user (from mesage column #4)
     find most seeing pair (maybe use Counter)
     """
-    ...
