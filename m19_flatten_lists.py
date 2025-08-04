caee73012a448f155a38ee67ba3344fafe2c34d8
alexander-romanov-V <romanovav@ya.ru>
2025-08-04 13:22:34 +0300

    282

--- m19_flatten_lists.py
+++ m19_flatten_lists.py
@@ -9,6 +9,8 @@ def de_flat(a_list):
             yield from de_flat(e)
         else:
             yield e
+
+
 def flatten(a_list):
     """Returns a list of lists (of any depth) returns a flattened version of it"""
     return list(de_flat(a_list))
@@ -26,7 +28,6 @@ def flatten3(a_list):
     if not isinstance(a_list, list):
         return [a_list]
     return [y for x in a_list for y in flatten3(x)]
-
 
 
 if __name__ == "__main__":
--- m21_adam_number.py
+++ m21_adam_number.py
@@ -20,7 +20,9 @@ if __name__ == "__main__":
     ]:
         assert p(31) is True
         print(f"{p.__name__:20} \033[92m[ PASS 1 ]\033[0m")
+
         assert p(22) is True
         print(f"{p.__name__:20} \033[92m[ PASS 2 ]\033[0m")
+
         assert p(15) is False
         print(f"{p.__name__:20} \033[92m[ PASS 3 ]\033[0m\n")
