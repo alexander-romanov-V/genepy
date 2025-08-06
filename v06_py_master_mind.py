988f889e5e262c904b0c8d63cdd5aef8f7eccac1
alexander-romanov-V <romanovav@ya.ru>
2025-08-06 15:29:18 +0300

    289

---
+++ v06_py_master_mind.py
@@ -0,0 +1,15 @@
+"""VERY HARD - Py Master Mind"""
+
+# Solution 1
+
+
+def gen_colors(code_size: int) -> str: ...
+
+
+def gen_code(code_size: int, colors: str) -> str: ...
+
+
+def check_guess(guess: str, code_size: int, colors: str) -> bool: ...
+
+
+print("Possible colors are ABCDEF\nCode is size 4.\nCODE = BFEB")
