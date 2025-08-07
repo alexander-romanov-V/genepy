8c77cf23538ba5c10472bcd79c45c81df79abf02
alexander-romanov-V <romanovav@ya.ru>
2025-08-07 10:42:37 +0300

    293

--- v06_py_master_mind.py
+++ v06_py_master_mind.py
@@ -2,6 +2,8 @@
 
 # Solution 1
 
+from random import choices
+
 
 def gen_colors(code_size: int) -> str:
     """Generate colors"""
@@ -16,6 +18,15 @@ def gen_code(code_size: int, colors: str) -> str:
 def check_guess(guess: str, code_size: int, colors: str) -> bool:
     """Check guess"""
     return len(guess) == code_size and set(guess).issubset(colors)
+
+
+def score_guess(code: str, guess: str) -> tuple:
+    """Score guess"""
+    exact = sum(1 for i in range(len(code)) if code[i] == guess[i])
+    partial = sum(
+        1 for i in range(len(code)) if code[i] != guess[i] and guess[i] in code
+    )
+    return (exact, partial)
 
 
 print("Possible colors are ABCDEF\nCode is size 4.\nCODE = BFEB")
