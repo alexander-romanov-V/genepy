53d7f5812fc9e35a7ffc1d2d3ae40f7d8074fa29
alexander-romanov-V <romanovav@ya.ru>
2025-08-07 10:41:48 +0300

    291

--- v06_py_master_mind.py
+++ v06_py_master_mind.py
@@ -3,12 +3,14 @@
 # Solution 1
 
 
-def gen_colors(code_size: int) -> str: ...
+def gen_colors(code_size: int) -> str:
     """Generate colors"""
     return "".join([chr(ord("A") + i) for i in range(min(26, max(0, code_size)))])
 
 
-def gen_code(code_size: int, colors: str) -> str: ...
+def gen_code(code_size: int, colors: str) -> str:
+    """Generate the code"""
+    return "".join(choices(colors, k=code_size))
 
 
 def check_guess(guess: str, code_size: int, colors: str) -> bool: ...
