2f58b2382a6017f5c71929a30fa277b11ce51e2f
alexander-romanov-V <romanovav@ya.ru>
2025-08-13 14:22:37 +0300

    312

--- m24_draw_n_squares.py
+++ m24_draw_n_squares.py
@@ -18,6 +18,5 @@ def draw_n_squares2(n):
 if __name__ == "__main__":
     for p in (draw_n_squares, draw_n_squares2):
         print(f"{p.__name__:20}")
-        print(p(1))
-        print(p(3))
-        print(p(5))
+        for n in (1, 3, 5):
+            print(p(n))
