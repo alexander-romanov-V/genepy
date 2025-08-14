33e7fcfff287d9b5af94c3c165f351487fa3481e
alexander-romanov-V <romanovav@ya.ru>
2025-08-14 13:25:41 +0300

    316

--- b09_import.py
+++ b09_import.py
@@ -4,9 +4,11 @@
 import math
 print(math.factorial(27))
 
+
 # Solution 2 - import with rename
 import math as m
 print(m.factorial(27))
+
 
 # Solution 3 - dynamic import
 print(__import__("math").factorial(27))
