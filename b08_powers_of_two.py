03c75d699d4c852332f5219210b313fdcd1f1452
alexander-romanov-V <romanovav@ya.ru>
2025-08-14 13:25:25 +0300

    315

--- b08_powers_of_two.py
+++ b08_powers_of_two.py
@@ -4,8 +4,10 @@
 for i in range(10):
     print(2**i)
 
+
 # Solution 2 - form a list and unpack it
 print(*(2**i for i in range(10)), sep="\n")
+
 
 # Solution 3 - for a list with side effects - calling print func
 l = [print(2**i) for i in range(10)]
