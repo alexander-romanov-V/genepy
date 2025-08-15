3012d2cc4824e4459ee699dcddb9240f5019be75
alexander-romanov-V <romanovav@ya.ru>
2025-08-15 11:49:29 +0300

    320

--- b12_fix_is_anagram.py
+++ b12_fix_is_anagram.py
@@ -1,11 +1,13 @@
 """BASIC - Fix is_anagram"""
 
+
 # Solution 1
 def is_anagram(left, right):
     """Returns True if left word is an anagram of right"""
     left_letters = sorted(left)
     right_letters = sorted(right)
     return left_letters == right_letters
+
 
 if __name__ == "__main__":
     print(is_anagram("rope", "pore"))
