6656caad314c4ff2783e5978853bf122b91d8ae5
alexander-romanov-V <romanovav@ya.ru>
2025-08-15 11:49:17 +0300

    319

--- b11_counting_words.py
+++ b11_counting_words.py
@@ -2,8 +2,10 @@
 
 whetting_your_appetite = "Python is an easy to learn, powerful programming language. It has efficient high level data structures and a simple but effective approach to object oriented programming. This tutorial introduces the reader informally to the basic concepts and features of the Python language and system. For a description of standard objects and modules..."
 
+
 # Solution 1
 print(len(whetting_your_appetite.split()))
+
 
 # Solution 2 - use RegEx
 print(len(__import__("re").findall(r"\w+", whetting_your_appetite)))
