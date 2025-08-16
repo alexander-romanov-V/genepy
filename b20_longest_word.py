e62e6554a42e2a8b3bd04c9458badc4af83d7616
alexander-romanov-V <romanovav@ya.ru>
2025-08-16 23:45:22 +0300

    327

--- b20_longest_word.py
+++ b20_longest_word.py
@@ -22,12 +22,7 @@ def longest_word3(text):
 
 
 if __name__ == "__main__":
-    print(longest_word("We want a SHRUBBERY"))
-    print(longest_word("Shrubberies are great"))
-
-    print(longest_word2("We want a SHRUBBERY"))
-    print(longest_word2("Shrubberies are great"))
-
-    print(longest_word3("We want a SHRUBBERY"))
-    print(longest_word3("Shrubberies are great"))
-
+    for p in (longest_word, longest_word2, longest_word3):
+        print(f"{p.__name__:20}")
+        print(p("We want a SHRUBBERY"))
+        print(p("Shrubberies are great"))
