1f9f4ee8ae5fa4ec7b263277d2b15216a030991f
alexander-romanov-V <romanovav@ya.ru>
2025-08-18 22:18:29 +0300

    333

--- h10_calculator3.py
+++ h10_calculator3.py
@@ -1,49 +1,7 @@
 """HARD - Calculator"""
 
-# Solution 1 - my first
-"""
-from sys import argv
-if len(argv) == 4:
-    try:
-        match argv[2]:
-            case "+":
-                print(int(argv[1]) + int(argv[3]))
-            case "-":
-                print(int(argv[1]) - int(argv[3]))
-            case "*":
-                print(int(argv[1]) * int(argv[3]))
-            case "/":
-                print(int(argv[1]) / int(argv[3]))
-            case "%":
-                print(int(argv[1]) % int(argv[3]))
-            case "^":
-                print(int(argv[1]) ** int(argv[3]))
-            case _:
-                raise ValueError
-    except ValueError:
-        print("input error")
-else:
-    print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
-"""
-
-
-# Solution 2 - my second
-from sys import argv
-if len(argv) == 4:
-    try:
-        if argv[2] not in "+-*/%^":
-            raise SyntaxError
-        if argv[2] == "^":
-            argv[2] = "**"
-        print(eval(''.join(argv[1:])))
-    except:
-        print("input error")
-else:
-    print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
-
-
-"""
 # Solution 3
+
 from sys import argv
 # import operator
 # operators = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv, "^": operator.pow, "%": operator.mod}
@@ -55,19 +13,3 @@ if len(argv) == 4:
         print("input error")
 else:
     print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
-
-
-# Solution 4 (but 5 / 2 is 2, not 2.5)
-from sys import argv
-prog, *arg = argv
-if len(arg) != 3:
-    print(f"usage: {prog} a_number (an_operator +-*/%^) a_number")
-else:
-    for o, po in zip("/^", ("//", "**")):
-        arg[1] = arg[1].replace(o, po)
-    try:
-        print(eval(" ".join(arg)))
-    except:
-        print("input error")
-
-"""
