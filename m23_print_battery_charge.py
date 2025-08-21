90c7fa8b5e320bac82eac43978d86d378eeb2be1
alexander-romanov-V <romanovav@ya.ru>
2025-08-21 15:37:27 +0300

    346

--- m23_print_battery_charge.py
+++ m23_print_battery_charge.py
@@ -1,10 +1,12 @@
 """MEDIUM - Product of iterable"""
 
 
+
 # Solution 1 - my
 def battery_charge(p: int) -> None:
     """Graphically represents a battery's charge"""
     print(f"[{'❚' * round(p / 10):<10}]\n{p}%")
+
 
 
 if __name__ == "__main__":
