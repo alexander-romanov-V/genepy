53dd48646f2be08ffa39c763ac23acbf6a3fcf66
alexander-romanov-V <romanovav@ya.ru>
2025-08-22 21:01:18 +0300

    349

--- m20_print_the_content_of_a_file_words.py
+++ m20_print_the_content_of_a_file_words.py
@@ -1,11 +1,13 @@
 """MEDIUM - Print the content of a file"""
 
 
+
 # Solution 1
 def print_file(file_name):
     """Print the content of a file"""
     with open(file_name, encoding="UTF-8") as file:
         print(file.read())
+
 
 
 # Solution 2
@@ -14,10 +16,12 @@ def print_file2(file_name):
     print(__import__("pathlib").Path(file_name).read_text())
 
 
+
 # Solution 3
 def print_file3(file_name):
     """Print the content of a file"""
     print(open(file_name, encoding="UTF-8").read())
+
 
 
 if __name__ == "__main__":    
