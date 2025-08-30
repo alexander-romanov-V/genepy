e453ae4c9f394e8ba6096470e565cf21be44339e
alexander-romanov-V <romanovav@ya.ru>
2025-08-30 16:22:04 +0300

    364

--- m14_currency.py
+++ m14_currency.py
@@ -26,6 +26,7 @@ def how_to_pay2(amount, currency):
 
 EURO = [1, 2, 5, 10, 20, 50, 100, 200, 500]
 
+
 if __name__ == "__main__":
     for p in (how_to_pay, how_to_pay2):
         print(f"{p.__name__:20}")
