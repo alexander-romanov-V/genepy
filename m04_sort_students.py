6dff6ffce1701eacbbadeb568be0dd5b70d6d983
alexander-romanov-V <romanovav@ya.ru>
2025-09-24 11:08:53 +0300

    405

--- m04_sort_students.py
+++ m04_sort_students.py
@@ -19,6 +19,7 @@ def sort_by_name(my_class):
     return sorted(my_class, key=lambda s: s[1])
 
 
+
 # Solution 2
 def sort_by_mark2(my_class):
     """Take a list of students and returns a copy of it sorted by mark in descending order"""
